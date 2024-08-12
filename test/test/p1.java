36caa6fee7cdbd9b631893729c2b9e78bbc50389
Alongkot <104056003+loko7@users.noreply.github.com>
2024-08-12 10:35:07 +0700

    update test static

---
+++ test/test/p1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class p1 {
+    public static void testStatic() {
+        System.out.println("static in opject");
+    }
+}
---
+++ test/test/test.java
@@ -0,0 +1,17 @@
+package test;
+
+public class test {
+	private int x = 5;
+
+	public static void testStatic() {
+		System.out.println("static in filed");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		testStatic();
+		p1.testStatic();
+	}
+
+}
